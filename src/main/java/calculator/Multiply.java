e0f0be6e8a511a42f274d01f624d7430cb690e08
96wilkinson <96wilkinson@sky.com>
2019-12-03 12:58:11 +0000

    added multiply feature (green)

--- src/main/java/calculator/Multiply.java
+++ src/main/java/calculator/Multiply.java
@@ -1,7 +1,7 @@
 package calculator;
 
 public class Multiply {
-	public double getResult(double num1, double num2) {
+	public static double getResult(double num1, double num2) {
 		return num1 * num2;
 	}
 
--- src/test/java/calculatorTest/MultiplyTest.java
+++ src/test/java/calculatorTest/MultiplyTest.java
@@ -11,14 +11,13 @@ public class MultiplyTest {
 
 	@Test
 	public void test() {
-		Multiply multiplyTest = new Multiply();
-		Assert.assertEquals(0, multiplyTest.getResult(10,0), 0 );
-		Assert.assertEquals(0, multiplyTest.getResult(0,10), 0 );
-		Assert.assertEquals(0, multiplyTest.getResult(0,0), 0 );
-		Assert.assertEquals(0, multiplyTest.getResult(10,10), 100 );
-		Assert.assertEquals(0, multiplyTest.getResult(2.5,2.5), 6.25 );
-		Assert.assertEquals(0, multiplyTest.getResult(100,0.5), 50 );
-		Assert.assertEquals(0, multiplyTest.getResult(100,1/4), 25 );
+		Assert.assertEquals(0, Multiply.getResult(10,0), 0 );
+		Assert.assertEquals(0, Multiply.getResult(0,10), 0 );
+		Assert.assertEquals(0, Multiply.getResult(0,0), 0 );
+		Assert.assertEquals(0, Multiply.getResult(10,10), 100 );
+		Assert.assertEquals(0, Multiply.getResult(2.5,2.5), 6.25 );
+		Assert.assertEquals(0, Multiply.getResult(100,0.5), 50 );
+		Assert.assertEquals(0, Multiply.getResult(100,1/4), 25 );
 
 	}
 }
