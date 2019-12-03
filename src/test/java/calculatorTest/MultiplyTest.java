05258ad0ff1a71580535c04d695ef8570c5965e7
96wilkinson <96wilkinson@sky.com>
2019-12-03 13:57:51 +0000

    added multiply feature (Refactor)

--- src/test/java/calculatorTest/MultiplyTest.java
+++ src/test/java/calculatorTest/MultiplyTest.java
@@ -7,17 +7,15 @@ import calculator.Multiply;
 
 public class MultiplyTest {
 
-
-
 	@Test
 	public void test() {
-		Assert.assertEquals(0, Multiply.getResult(10,0),0);
-		Assert.assertEquals(0, Multiply.getResult(0,10),0);
-		Assert.assertEquals(0, Multiply.getResult(0,0),0);
-		Assert.assertEquals(100, Multiply.getResult(10,10),1);
-		Assert.assertEquals(6.25 , Multiply.getResult(2.5,2.5),1);
-		Assert.assertEquals(50, Multiply.getResult(100,0.5),1);
-		Assert.assertEquals(25, Multiply.getResult(100,0.25),1);
-		Assert.assertEquals(-100, Multiply.getResult(100,-1),0);
+		Assert.assertEquals(0, Multiply.getResult(10, 0), 0.0001);
+		Assert.assertEquals(0, Multiply.getResult(0, 10), 0.0001);
+		Assert.assertEquals(0, Multiply.getResult(0, 0), 0.0001);
+		Assert.assertEquals(100, Multiply.getResult(10, 10), 0.0001);
+		Assert.assertEquals(6.25, Multiply.getResult(2.5, 2.5), 0.0001);
+		Assert.assertEquals(50, Multiply.getResult(100, 0.5), 0.0001);
+		Assert.assertEquals(25, Multiply.getResult(100, 0.25), 0.0001);
+		Assert.assertEquals(-100, Multiply.getResult(100, -1), 0.0001);
 	}
 }
