db3b16cb5bf90dc1ecbdce3d8b726036e2e05457
euan779 <euan779@gmail.com>
2019-12-03 14:06:36 +0000

    Refactored add

--- src/main/java/calculator/Add.java
+++ src/main/java/calculator/Add.java
@@ -2,7 +2,6 @@ package calculator;
 
 public class Add {
     public static double getResult(double num1, double num2){
-        double result = num1 + num2;
-        return result;
+        return num1 + num2;
     }
 }
--- src/test/java/calculatorTest/AddTest.java
+++ src/test/java/calculatorTest/AddTest.java
@@ -1,7 +1,5 @@
 package calculatorTest;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,12 +7,10 @@ import calculator.Add;
 
 public class AddTest {
 
-	Add add = new Add();
-
 	@Test
 	public void test() {
-		Assert.assertEquals((add.getResult(5,5)), 10, 0.00005);
-		Assert.assertNotEquals((add.getResult(5, 5)), 11, 0.00005);
+		Assert.assertEquals((Add.getResult(5,5)), 10, 0.00005);
+		Assert.assertNotEquals((Add.getResult(5, 5)), 11, 0.00005);
 	}
 
 }
