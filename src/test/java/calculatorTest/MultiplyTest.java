b8ae20126fff032e5616749ff1ddd7f0cb7481d9
96wilkinson <96wilkinson@sky.com>
2019-12-03 13:04:49 +0000

    added multiply feature (green)

--- src/test/java/calculatorTest/MultiplyTest.java
+++ src/test/java/calculatorTest/MultiplyTest.java
@@ -11,13 +11,13 @@ public class MultiplyTest {
 
 	@Test
 	public void test() {
-		Assert.assertEquals(0, Multiply.getResult(10,0), 0 );
-		Assert.assertEquals(0, Multiply.getResult(0,10), 0 );
-		Assert.assertEquals(0, Multiply.getResult(0,0), 0 );
-		Assert.assertEquals(0, Multiply.getResult(10,10), 100 );
-		Assert.assertEquals(0, Multiply.getResult(2.5,2.5), 6.25 );
-		Assert.assertEquals(0, Multiply.getResult(100,0.5), 50 );
-		Assert.assertEquals(0, Multiply.getResult(100,1/4), 25 );
-
+		Assert.assertEquals(0, Multiply.getResult(10,0),0);
+		Assert.assertEquals(0, Multiply.getResult(0,10),0);
+		Assert.assertEquals(0, Multiply.getResult(0,0),0);
+		Assert.assertEquals(100, Multiply.getResult(10,10),1);
+		Assert.assertEquals(6.25 , Multiply.getResult(2.5,2.5),1);
+		Assert.assertEquals(50, Multiply.getResult(100,0.5),1);
+		Assert.assertEquals(25, Multiply.getResult(100,0.25),1);
+		Assert.assertEquals(-100, Multiply.getResult(100,-1),0);
 	}
 }
