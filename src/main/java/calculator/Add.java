74e0e22642d4b2ccc03d3f9de812b915d55aed7d
euan779 <euan779@gmail.com>
2019-12-03 13:59:01 +0000

    add function

--- src/main/java/calculator/Add.java
+++ src/main/java/calculator/Add.java
@@ -1,5 +1,8 @@
 package calculator;
 
 public class Add {
-
+    public static double getResult(double num1, double num2){
+        double result = num1 + num2;
+        return result;
+    }
 }
--- src/test/java/calculatorTest/AddTest.java
+++ src/test/java/calculatorTest/AddTest.java
@@ -2,13 +2,19 @@ package calculatorTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import calculator.Add;
 
 public class AddTest {
 
+	Add add = new Add();
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Assert.assertEquals((add.getResult(5,5)), 10, 0.00005);
+		Assert.assertNotEquals((add.getResult(5, 5)), 11, 0.00005);
 	}
 
 }
